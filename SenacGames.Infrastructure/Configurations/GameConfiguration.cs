402696b824738a515b5d7a88abc1005317f76f78
LUCAS <lucarty79@gmail.com>
2026-05-29 17:32:12 -0300

    atulização de category adicionando context e mais classes chama Configuration

---
+++ SenacGames.Infrastructure/Configurations/GameConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SenacGames.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SenacGames.Infrastructure.Configurations
+{
+    internal class GameConfiguration : IEntityTypeConfiguration<Game>
+    {
+        public void Configure(EntityTypeBuilder<Game> builder)
+        {
+            builder.HasKey(g => g.Id);
+
+            builder.Property(g => g.Title)
+                .IsRequired()
+                .HasMaxLength(200);
+
+            builder.Property(g => g.Description)
+                .HasMaxLength(2000);
+
+            builder.Property(g => g.CoverImageUrl)
+                .HasMaxLength (500);
+
+            builder.HasOne(g => g.Category) // um gamer tem uma categoria 
+                .WithMany(c => c.Games) //uma categoria tem muitos games
+                .HasForeignKey(g => g.Category) // a FK é CategoryId
+            .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
